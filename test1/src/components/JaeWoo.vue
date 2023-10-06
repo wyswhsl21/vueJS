284fd00a8600bdf88809eb38ad36d5758d54f558
unknown <wyswhsl21@naver.com>
2023-10-06 23:40:47 +0900

    vuejs 싱글파일컴포넌트

---
+++ test1/src/components/JaeWoo.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <p>{{ name }}</p>
+    <button @click="updateName">이름 바꾸기</button>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      name: 'Vue 익히기',
+    };
+  },
+
+  methods: {
+    updateName() {
+      this.name = 'Vue 싱글파일 컴포넌트';
+    },
+  },
+};
+</script>
+
+<style scoped>
+h1 {
+  color: red;
+}
+</style>
--- test1/src/views/HomeView.vue
+++ test1/src/views/HomeView.vue
@@ -1,18 +1,28 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <h1>This is Home page</h1>
+    <p>{{ name }}</p>
+    <JaeWoo />
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
+import JaeWoo from '@/components/JaeWoo.vue';
 export default {
-  name: 'HomeView',
   components: {
-    HelloWorld
-  }
-}
+    JaeWoo,
+  },
+  data() {
+    return {
+      name: 'VueItem',
+    };
+  },
+};
 </script>
+// scoped 넣어줘야됨.
+<style scoped>
+h1 {
+  color: red;
+  font-size: 20px;
+}
+</style>
